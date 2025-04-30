abe004c501c1e26b4440b8e54efbb266f1ef551e
elyass999s <elyastaheri999@gmail.com>
2025-04-30 15:44:19 -0700

    Add files via upload

---
+++ style.js
@@ -0,0 +1,22 @@
+window.addEventListener('load', () => {
+    const loader = document.getElementById('loader');
+    loader.style.opacity = '0';
+    loader.style.pointerEvents = 'none';
+    setTimeout(() => loader.remove(), 1000);
+  });
+
+
+
+
+
+
+
+
+
+
+  window.addEventListener("load", () => {
+    const loader = document.getElementById("loader");
+    loader.style.opacity = "0";
+    loader.style.transition = "opacity 0.5s ease";
+    setTimeout(() => loader.style.display = "none", 500);
+  });
